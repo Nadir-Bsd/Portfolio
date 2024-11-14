e0df637a45fc0aebfa8b9b6ec776da5eecab70d0
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-14 23:11:55 +0100

    UPDATE: links, name, head title, picture

--- src/components/RepoList/RepoList.tsx
+++ src/components/RepoList/RepoList.tsx
@@ -5,65 +5,75 @@ import Alert from "../ui/Alert/Alert";
 import "./RepoList.styles.scss";
 
 type PinnedRepos = {
-  owner: string;
-  repo: string;
-  link: string;
-  description: string;
-  image: string;
-  language: string;
-  languageColor: string;
-  stars: number;
-  forks: number;
+    owner: string;
+    repo: string;
+    link: string;
+    description: string;
+    image: string;
+    language: string;
+    languageColor: string;
+    stars: number;
+    forks: number;
 };
 
 export default function RepoList() {
-  const [repos, setRepos] = useState<PinnedRepos[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  // tu dois comprendre ca
+    const [repos, setRepos] = useState<PinnedRepos[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+  // tu dois comprendre ca
   useEffect(() => {
-    const fetchRepos = async () => {
-      try {
-        const response = await fetch(
-          "https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=stekatag"
-        );
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        if (data.length === 0) {
-          throw new Error("No pinned repos found");
-        }
-        setRepos(data);
-      } catch (error: any) {
-        if (error.message === "Network response was not ok") {
-          setError("Your network may be down. Please try again.");
-        } else if (error.message === "No pinned repos found") {
-          setError("No pinned repos found");
-        } else {
-          setError("Error fetching repos");
-        }
-        console.error("Error fetching repos:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
 
-    fetchRepos();
-  }, []);
+      // tu dois etre sur d'avoir compris ca
+        const fetchRepos = async () => {
+            // tu dois comprendre ca
+            try {
+                // tu dois etre sur d'avoir compris ca
+                const response = await fetch(
+                    "https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=stekatag"
+                );
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                if (data.length === 0) {
+                    throw new Error("No pinned repos found");
+                }
+                setRepos(data);
+            } catch (error: any) {
+                if (error.message === "Network response was not ok") {
+                    setError("Your network may be down. Please try again.");
+                } else if (error.message === "No pinned repos found") {
+                    setError("No pinned repos found");
+                } else {
+                    setError("Error fetching repos");
+                }
+                console.error("Error fetching repos:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-  return (
-    <>
-      <div className="repos">
-        {loading ? (
-          <div className="spinner-wrapper">
-            <h4>Loading projects</h4>
-            <Spinner />
-          </div>
-        ) : (
-          repos.map((repo) => <RepoCard key={repo.repo} {...repo} />)
-        )}
-      </div>
-      {error && <Alert message={error} />}
-    </>
-  );
-}
+        fetchRepos();
+    }, []);
+
+    // tu dois etre sur d'avoir compris ca
+    return (
+
+        // tu dois comprendre ca
+        <>
+            <div className="repos">
+                {loading ? (
+                    <div className="spinner-wrapper">
+                        <h4>Loading projects</h4>
+                        <Spinner />
+                    </div>
+                ) : (
+                    repos.map((repo) => <RepoCard key={repo.repo} {...repo} />)
+                )}
+            </div>
+            {error && <Alert message={error} />}
+        </>
+    );
+} 
